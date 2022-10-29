f798e790b4b1fbfb115a42bf69c80fa7bb27eb8b
Pavithiran2000 <94287015+Pavithiran2000@users.noreply.github.com>
2022-10-30 04:09:13 +0530

    moved the drivers to a separate folder

--- kmain.c
+++ kmain.c
@@ -1,13 +1,14 @@
-    #include "frame_buffer.h"
-    #include "serial_port.h"
-
+    #include "drivers/frame_buffer.h"
+    #include "drivers/serial_port.h"
 
 
     
     int main(){
 
-           char ptr2[] = "Welcome to Black OS";
+           char ptr2[] = "Welcome to CarbonOS";
    
+    
     serial_write(0x3F8, ptr2, 19);
-    fb_write( ptr2, 19);
+    fb_write(ptr2, 19);
+    
     }
